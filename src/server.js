bf16315846e18fbf87e2610370034feb35be6946
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-05 19:40:34 -0700

    Use express-session in server to make cookies

--- src/server.js
+++ src/server.js
@@ -2,10 +2,19 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+const session = require('express-session')
 
 const port = process.env.PORT || 3000
 
 const app = express()
+
+const sessionOptions = {
+  name: 'session',
+  secret: 'Chamber of Secrets',
+  cookie: {maxAge: 1000 * 60 * 60 * 24},
+  resave: false,
+  saveUninitialized: false,
+}
 
 require('ejs')
 app.set('view engine', 'ejs')
@@ -13,6 +22,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(session(sessionOptions))
 
 app.use('/', routes)
 
