cf269a26ac586a948b7cbb332e1f88113f15221b
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-06 11:11:40 -0700

    Add middleware function that's console logging to make sure it's working

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 const nonUser = require('./non-user')
 const user = require('./user-only')
 
+router.use((req, res, next) => {
+  console.log('middleware stuff')
+  next()
+})
+
 router.use('/', nonUser)
 router.use('/', user)
 
