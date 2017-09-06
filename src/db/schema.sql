b4d4a8e2c9929643f79c2465b1ee665931c31959
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-05 17:36:24 -0700

    Add user table to schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE albums (
   title VARCHAR(255) NOT NULL,
   artist VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE users (
+  user_id SERIAL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  join_date DATE DEFAULT CURRENT_DATE,
+  photo VARCHAR(255) DEFAULT '/pictures/profile.jpg'
+);
