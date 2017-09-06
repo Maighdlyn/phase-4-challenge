3ef6b9b2dbf3c6d88d7cd17eda3377e636ed986f
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-05 19:19:53 -0700

    Remove pg references from queries.js as they're no longer needed with pg-promise

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,12 +1,8 @@
-const pg = require('pg')
 const pgp = require('pg-promise')()
 
 const dbName = 'vinyl'
 const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
-const client = new pg.Client(connectionString)
 const db = pgp(connectionString)
-
-client.connect()
 
 const getAlbums = () => {
   return db.query('SELECT * FROM albums')
